a71b0f5a6941989addb37212522306f693b45771
Ranjeet Rathour <ranjeet.rathour@moptra.com>
2026-03-23 11:05:28 +0100

    test pre condition

--- src/main/resources/db/changelog/sql/001-create-tables.sql
+++ src/main/resources/db/changelog/sql/001-create-tables.sql
@@ -1,10 +1,14 @@
 --liquibase formatted sql
 
 --changeset ranjeet-rathour:001
---precondition-sql-check exceptedResult:0 select count(*) from information.schema.tables where table_name="school"
+--precondition-sql-check expectedResult:0 SELECT COUNT(*) FROM information_schema.tables WHERE table_name='school'
+
 CREATE TABLE IF NOT EXISTS school
 (
     id   BIGINT PRIMARY KEY,
     name VARCHAR(255),
     city VARCHAR(255)
-);
+    );
+
+-- changeset ranje:1774073410776-1
+INSERT INTO school (id, name, city) VALUES (1, 'a', 'a');
