29e17e832b5eab1586d1f2e0cef0248f0c530782
Ranjeet Rathour <ranjeet.rathour@moptra.com>
2026-03-23 10:45:12 +0100

    add pre condition

--- src/main/resources/db/changelog/sql/001-create-tables.sql
+++ src/main/resources/db/changelog/sql/001-create-tables.sql
@@ -1,6 +1,7 @@
 --liquibase formatted sql
 
 --changeset ranjeet-rathour:001
+--precondition-sql-check exceptedResult:0 select count(*) from information.schema.tables where table_name="school"
 CREATE TABLE IF NOT EXISTS school
 (
     id   BIGINT PRIMARY KEY,
