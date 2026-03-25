22acfc04f1767dbc449d0d6f7a2afa19291e3683
Ranjeet Rathour <ranjeet.rathour@moptra.com>
2026-03-25 07:55:34 +0100

    add column with precondition

--- src/main/resources/db/changelog/sql/001-create-tables.sql
+++ src/main/resources/db/changelog/sql/001-create-tables.sql
@@ -10,5 +10,12 @@ CREATE TABLE IF NOT EXISTS school
     city VARCHAR(255)
     );
 
+--changeset ranjeet-rathour:002
+--preconditions onFail:MARK_RAN
+--precondition-sql-check expectedResult:0 SELECT COUNT(*) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'liquibase_db' AND TABLE_NAME = 'school' AND COLUMN_NAME = 'student_type';
+ALTER TABLE school
+    ADD COLUMN student_type VARCHAR(50);
+
+
 -- changeset ranje:1774073410776-1
-INSERT INTO school (id, name, city) VALUES (1, 'a', 'a');
+-- INSERT INTO school (id, name, city) VALUES (1, 'a', 'a');
